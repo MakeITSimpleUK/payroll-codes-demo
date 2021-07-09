b21c799bf6604a750448f7c1698060d08b3b13e0
Wahba91 <zizo@makeitsimple.co.uk>
2021-07-09 14:15:48 +0200

    payrollData v2

---
+++ src/models/PayrollData-v2.js
@@ -0,0 +1,62 @@
+/**
+ * Interface for PayrollData
+ * Default values
+ */
+module.exports = payrollData = {
+  filters: {
+    payFrequency: 'Weekly',
+    agency: ['agency1_id', 'agency2_id'],
+    product: 'Umbrella'
+  },
+
+  payroll: {
+    'Pay Frequency': '' //Weekly, Monthly, etc.
+  },
+
+  system: {
+    statutoryRate: {}
+  },
+
+  workers: ['candidate1_id', 'candidate2_id', 'candidate3_id'],
+
+  workersInfo: {
+    'candidate1_id': {
+
+      // contains candidates' objects
+      info: {
+        'Pension Applicable': '',
+        'Service Used': '',
+        'Holiday Pay Rule': '',
+        'Apprenticeship Levy Applicable': ''
+      },
+
+      timesheets: [{}],
+
+      actionRequests: [{}],
+
+      // all other items
+
+
+      // Results
+      payslip: {
+        'Gross Timesheet Value': 0,
+        'Margin': 0,
+        'Holiday Pay': 0,
+        'Employer NI': 0,
+        'Employer Pension': 0,
+        'Apprenticeship Levy': 0,
+        'Gross Pay For Tax': 0,
+        'Tax': 0,
+        'Employee NI': 0,
+        'Employee Pension': 0,
+        'Net Pay': 0,
+      },
+
+      errors: [{
+
+      }]
+
+    }
+  }
+
+};
